e212406e25c2e7022dbfde5a3b9144930ec1df08
Manuel-hub-s <83520897+Manuel-hub-s@users.noreply.github.com>
2024-12-04 16:06:42 -0600

    Update codigo.py
    Primeros cambios del código

--- codigo.py
+++ codigo.py
@@ -15,7 +15,7 @@ Puma_concolor = 'Puma/puma_concolor.csv'  # CSV de Puma concolor
 lim_provincias = 'Puma/provincias.gpkg'  # Archivo de límites provinciales
 
 # %% [markdown]
-st.title('Puma concolor: Costa Rica')
+st.title('Puma concolor en Costa Rica')
 st.subheader('Manuel Peralta Reyes')
 
 # %% [markdown]
@@ -49,57 +49,47 @@ def cargar_lim_provincias():
         if provincias.crs is None:
             provincias.set_crs("EPSG:4326", inplace=True)
         return provincias
-    except Exception as e:
-        st.error(f"Error al cargar los datos geoespaciales: {e}")
-        return None
 
 # %% [markdown]
 # ## Cargar los datos
 
 # Cargar datos de Ara ambiguus
-MacaoAmbiguus_CR = cargar_Ara_MacaoAmbiguus()
-
-#if MacaoAmbiguus_CR is not None:
-    #st.write("Datos filtrados (Ara ambiguus) cargados con éxito.")
-    #st.dataframe(MacaoAmbiguus_CR.head())  # Mostrar primeras filas
-#else:
-    #st.error("No se pudieron cargar los datos.")
-    #st.stop()
+Pum_concolor = cargar_Puma_concolor()
 
 # Cargar datos geoespaciales de las provincias
-#carga_provinciasCR = st.text('Cargando datos de los límites de las provincias...')
+carga_provinciasCR = st.text('Cargando datos de los límites de las provincias...')
 provinciasCR = cargar_lim_provincias()
-#carga_provinciasCR.text('Los límites de las provincias han sido cargados.')
+carga_provinciasCR.text('Los límites de las provincias han sido cargados.')
 
 #if provinciasCR is not None:
-    #st.write("Columnas disponibles en provinciasCR:", provinciasCR.columns.tolist())
-#else:
-    #st.error("No se pudieron cargar las provincias.")
-    #st.stop()
+    st.write("Columnas disponibles en provinciasCR:", provinciasCR.columns.tolist())
+else:
+    st.error("No se pudieron cargar las provincias.")
+    st.stop()
 
 # %% [markdown]
 # ## Agrupamiento por provincia
 
 @st.cache_data
-def agrupar_por_provincia(MacaoAmbiguus_CR):
-    agrupado = MacaoAmbiguus_CR.groupby('Provincia')['Cuenta Individual'].sum().reset_index()
-    agrupado.rename(columns={'Cuenta Individual': 'Total Cuenta Individual'}, inplace=True)
+def Puma_provincia(Pum_concolor):
+    agrupado =Pum_concolor.groupby('Provincia')['Cuenta Individual'].sum().reset_index()
+    agrupado.rename(columns={'Cuenta Individual': 'Total avistamientos'}, inplace=True)
     return agrupado
 
 # Agrupamiento por provincia
-agrupado_MacaoAmbiguus_CR = agrupar_por_provincia(MacaoAmbiguus_CR)
+agrup_Pum_concolor = agrupar_por_provincia(Pum_concolor)
 
 # Mostrar los totales por provincia
-#st.subheader('Totales por provincia (Ara ambiguus)')
-#st.dataframe(agrupado_MacaoAmbiguus_CR)
+#st.subheader('Totales por provincia')
+#st.dataframe(agrup_Pum_concolor)
 
 # %% [markdown]
 # ## Seleccionador por Provincia
 
 # Generar la lista de provincias y agregar "Todas"
-MacAmb_prov = agrupado_MacaoAmbiguus_CR['Provincia'].tolist()
-MacAmb_prov.sort()
-opciones_provincias = ['Todas'] + MacAmb_prov
+Puma_prov = agrup_Pum_concolor['Provincia'].tolist()
+Puma_prov.sort()
+opciones_provincias = ['Todas'] + Puma_prov
 
 provincia_seleccionada = st.sidebar.selectbox(
     'Selecciona una provincia:',
@@ -108,30 +98,29 @@ provincia_seleccionada = st.sidebar.selectbox(
 
 # Filtrar los datos según la selección
 if provincia_seleccionada != 'Todas':
-    datos_filtrados = MacaoAmbiguus_CR[MacaoAmbiguus_CR['Provincia'] == provincia_seleccionada]
-    datos_filtrados_agrupados = agrupado_MacaoAmbiguus_CR[
-        agrupado_MacaoAmbiguus_CR['Provincia'] == provincia_seleccionada
+    datos_filtrados = Pum_concolor[Pum_concolor['Provincia'] == provincia_seleccionada]
+    datos_filtrados_agrupados = Pum_concolor[Pum_concolor['Provincia'] == provincia_seleccionada
     ]
 else:
-    datos_filtrados = MacaoAmbiguus_CR.copy()
-    datos_filtrados_agrupados = agrupado_MacaoAmbiguus_CR.copy()
+    datos_filtrados = Pum_concolor.copy()
+    datos_filtrados_agrupados = Pum_concolor.copy()
 
 # Mostrar los datos filtrados
-st.subheader(f'Datos filtrados para: {provincia_seleccionada}')
+st.subheader(f'Datos para la provincia {provincia_seleccionada}')
 st.dataframe(datos_filtrados)
 
 # %% [markdown]
 # ## Gráfico de Totales por Provincia
 
-fig_totales = px.bar(
+graf = px.bar(
     datos_filtrados_agrupados,
-    x='Provincia',
+    x='Especie',
     y='Total Cuenta Individual',
-    title=f'Totales de Ara ambiguus para {provincia_seleccionada}',
-    labels={'Provincia': 'Provincia', 'Total Cuenta Individual': 'Total Cuenta Individual'},
+    title=f'Totales de felinos para {provincia_seleccionada}',
+    labels={'Especie': 'Especie', 'Total Cuenta Individual': 'Total'},
     color='Provincia'
 )
-st.plotly_chart(fig_totales)
+st.plotly_chart(graf)
 
 # %% [markdown]
 # ## Mapa de Totales por Provincia
